094bb1c28713a7e94eb61a11da8ae02f846d4ca4
bajadevmx <91626603+bajadevmx@users.noreply.github.com>
2024-06-09 02:31:39 -0700

    Add files via upload

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,19 +1,18 @@
 // // STEP 1
 // function Cat() {
+//     constructor() 
 
 // }
 
 // function Dog() {
-
+//     constructor() 
+    
 // }
+
 // // STEP 2
-// const newCat =function Cat() {
+// const newCat =new Cat('Macarena') 
+// const newDog= new Dog('Cookie') 
 
-// }
-
-// const newDog= function Dog() {
-
-// }
 
 // // STEP 3
 // function Animal(displayMsg) {
@@ -31,28 +30,82 @@
 // console.log(Cname.animalName)				
 
 
-// STEP 5
-class Animal {
-    constructor(type,breed, color, height, lenght) {
-        this._type = type
-        this._breed = breed
-        this._color = color
-        this._height = height
-        this._lenght = lenght
-    }
-}
+// // STEP 5
+// class Animal {
+//     constructor(type,breed, color, height, lenght) {
+//         this._type = type
+//         this._breed = breed
+//         this._color = color
+//         this._height = height
+//         this._lenght = lenght
+//     }
+// }
 
-const Cookie= new Animal('Dog','Maltipoo','White','6in','10in')
+// const Cookie= new Animal('Dog','Maltipoo','White','6in','10in')
 
-// STEP 6
-for (const props in Cookie) {
-    console.log(`${Animal[props]}`)
-}
+// // STEP 6
+// for (const props in Cookie) {
+//     console.log(`${Animal[props]}`)
+// }
 
-// STEP 7
-class Speak{
-    
-}
-// STEP 8
+// // STEP 7
+// class Animal {
+//     constructor(type,breed, color, height, lenght) {
+//         this._type = type //properties (atributes)
+//         this._breed = breed
+//         this._color = color
+//         this._height = height
+//         this._lenght = lenght
+//     }
+//     Speak() {
+//         if (this._type== 'Dog') {
+//             return `The ${this._color} dog is barking!`
+//         } else {
+//             return `The ${this._color} cat is meowing!`
+//         }
+        
+//     }
+// }
+// let Cookie= new Animal('Dog','Maltipoo','White','6in','10in')
+// console.log(Cookie.Speak())
 
-// STEP 9
+// // STEP 8
+// class Animal {
+//     constructor(type,breed, color, height, lenght) {
+//         this.type = type //properties (atributes)
+//         this.breed = breed
+//         this.color = color
+//         this.height = height
+//         this.lenght = lenght
+//     }
+//     Speak() {
+//         if (this.type== 'Dog') {
+//             return `The ${this.color} dog is barking!`
+//         } else {
+//             return `The ${this.color} cat is meowing!`
+//         }
+//     }
+//     checkType() {
+//         if (this.type== 'Dog') {
+//             return `The ${this.type} has made a noise!`
+//         } else {
+//             return `The ${this.type} has made a noise!`
+//         }
+
+//     }
+// }
+// let Cookie= new Animal('Dog','Maltipoo','White','6in','10in')
+// console.log(Cookie.checkType())
+
+
+// // STEP 9
+// const str= "The dog has been selectively bred over millennia for various behaviors, sensory capabilities, and physical attributes.[5] Dog breeds vary widely in shape, size, and color. They perform many roles for humans, such as hunting, herding, pulling loads, protection, assisting police and the military, companionship, therapy, and aiding disabled people"
+// function findWord(word) {
+//     let numwrds= str.split(' ')
+//     for (let i = 0 , i < numwrds, i++) {
+//         if numwrds[i] = word {
+//             let twrds= twrds++
+//         }
+//     }
+// }
+// console.log(findWord('dog'))
